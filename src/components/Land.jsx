a06f8efc4cc068a87ab862db31111b62aee3e5f6
DJ-BoT07 <djmangaonkar7@gmail.com>
2025-01-22 02:23:22 +0530

    Netlify.toml 4

--- src/components/Land.jsx
+++ src/components/Land.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { LampContainer } from "./Lamp";
+import { LampContainer } from "./Lamp.jsx";
 import CarouselDemo from "./Carousel";
 import { useNavigate } from 'react-router-dom';
 
